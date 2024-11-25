782497660c4dd46881b57abfccc8f0fdcc58d842
Broc Keeton <broc.keeton@student.centralia.edu>
2024-11-25 11:52:41 -0800

    Added User borrow history.

--- Library Management System/Program.cs
+++ Library Management System/Program.cs
@@ -5,8 +5,19 @@ Console.WriteLine("| Welcome to the Library Management System |");
 
 LibraryDatabaseManagment libraryDatabaseManagment = new LibraryDatabaseManagment();
 
-libraryDatabaseManagment.AddBook(new Book("The Great Gatsby", "F. Scott Fitzgerald", "1234", 5));
-libraryDatabaseManagment.AddBook(new Book("1984", "George Orwell", "2345", 2));
+var book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1234", 5);
+var book2 = new Book("1984", "George Orwell", "2345", 2);
+var book3 = new Book("To Kill a MockingBird", "Harper Lee", "3456", 4);
+var book4 = new Book("War and Peace", "Leo Tolstoy", "4567", 3);
+
+libraryDatabaseManagment.AddBook(book1);
+libraryDatabaseManagment.AddBook(book2);
+libraryDatabaseManagment.AddBook(book3);
+libraryDatabaseManagment.AddBook(book4);
 libraryDatabaseManagment.DisplayCatalog();
 libraryDatabaseManagment.RemoveBook("1234");
 libraryDatabaseManagment.DisplayCatalog();
+
+var user = new User("Broc");
+
+user.BorrowBook(book1);
---
+++ Library Management System/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public Stack<Book> BorrowingHistory { get; set; }
+
+        public User(string name)
+        {
+            Name = name;
+            BorrowingHistory = new Stack<Book>();
+        }
+
+        public bool BorrowBook(Book book)
+        {
+            if (book.AvailableCopies > 0)
+            {
+                BorrowingHistory.Push(book);
+                book.AvailableCopies--;
+                Console.WriteLine($"{Name} borrowed: {book.Title}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{book.Title} is currently unavailable.");
+                return false;
+            }
+        }
+        
+        public bool ReturnBook()
+        {
+            if (BorrowingHistory.Count > 0)
+            {
+                Book book = BorrowingHistory.Pop();
+                book.AvailableCopies++;
+                Console.WriteLine($"{Name} returned: {book.Title}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} has no books to return.");
+                return false;
+            }
+        }
+
+        public void ViewBorrowoingHistory()
+        {
+            Console.WriteLine($"\n{Name}'s Borrowing History:");
+            if (BorrowingHistory.Count == 0)
+            {
+                Console.WriteLine("No books borrowed.");
+            }
+            else
+            {
+                foreach (var book in BorrowingHistory)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+        }
+    }
+}
